1bc2f2d38fa4306f43578ebf40987a4285ca8949
carlos <crls.andres.gb@gmail.com>
2020-08-12 10:58:42 -0600

    create project and add file for home

---
+++ src/homepage.component.jsx
@@ -0,0 +1,48 @@
+/**
+ * Import Core
+ */
+import React from "react";
+/**
+ * import css
+ */
+import "./homepage.styles.scss";
+/**
+ * Main component
+ */
+
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
