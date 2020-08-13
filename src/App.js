b4616d16afeb88cf46ac1763b70839920e8805ae
carlos <crls.andres.gb@gmail.com>
2020-08-13 08:37:01 -0600

    update app with shop page, collection preview and items components

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import { Route, Switch } from "react-router-dom";
  */
 import "./App.css";
 /**
- * import components
+ * import Pages
  */
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       {/**set router section, we use switch to handle first found */}
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
