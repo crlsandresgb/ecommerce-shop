1075455bdec6978d78be924139d3945b3b760624
carlos <crls.andres.gb@gmail.com>
2020-08-20 14:20:10 -0600

    create dropdown component and the item inside usign reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@
  * import core
  */
 import React from "react";
+import { connect } from "react-redux";
 /**
  * import css
  */
@@ -10,12 +11,22 @@ import "./cart-dropdown.styles.scss";
  * import component
  */
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
-
-export default CartDropdown;
+/**
+ * map props
+ */
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,24 @@
+/**
+ * import core
+ */
+import React from "react";
+/**
+ * import css
+ */
+import "./cart-item.styles.scss";
+/**
+ * functional component
+ */
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="item" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
