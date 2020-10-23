16c1c62af411a7f8878d3e63246e890933506a16
carlos <crls.andres.gb@gmail.com>
2020-10-23 12:07:52 -0600

    create backend server and client

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,48 @@
+/**
+ * import core
+ */
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishabelKey = "pk_test_cKyyalXdiZE8SOT8bx5Ma5Qb00LKlUnGd4";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Succesful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please use the provided credit card"
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name=" React eCommerce"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishabelKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
